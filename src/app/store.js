bbdaf4d6ab75f84ad257de74241b9a24e8c6688f
Chudamani Lawerence <101648817+cmlowerence@users.noreply.github.com>
2026-01-05 12:33:52 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/authSlice';
 
 export const store = configureStore({
   reducer: {
-    // We will add slices (user, clan, etc.) here later
+    auth: authReducer,
   },
 });
