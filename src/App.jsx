97c4756c3d4e08fddf5e7454f0a595a81e1a1fbc
Chudamani Lawerence <101648817+cmlowerence@users.noreply.github.com>
2026-01-05 12:44:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Search from './pages/Search';
+
 function App() {
   return (
     <div className="min-h-screen bg-neutral-900">
@@ -17,5 +19,3 @@ function App() {
 }
 
 export default App;
-
-
