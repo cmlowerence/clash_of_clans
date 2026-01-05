e345ab87919f36227e42047e5d17d033f92da918
Chudamani Lawerence <101648817+cmlowerence@users.noreply.github.com>
2026-01-05 19:15:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Search from './pages/Search';
-import ClanDetails from './pages/ClanDetails'; // Import new page
+import ClanDetails from './pages/ClanDetails';
+import PlayerDetails from './pages/PlayerDetails';
 
 function App() {
   return (
@@ -14,11 +15,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/search" element={<Search />} />
-        {/* The :tag part is dynamic, it will grab whatever tag we click */}
         <Route path="/clan/:tag" element={<ClanDetails />} />
+        <Route path="/player/:tag" element={<PlayerDetails />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
