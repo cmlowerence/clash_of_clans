ca3a83c458574d3466b318d0b1dbe61c0b3aca1b
Chudamani Lawerence <101648817+cmlowerence@users.noreply.github.com>
2026-01-05 12:30:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Any request starting with /api will be forwarded to Clash of Clans
+      '/api': {
+        target: 'https://api.clashofclans.com/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      },
+    },
+  },
 })
+
